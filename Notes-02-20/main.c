add5f899b0f1bf34cfc93dbf994670936a71dab0
Aaron Block <ablock@austincollege.edu>
2019-02-20 09:31:47 -0600

    Basic Pointers

--- Notes-02-20/main.c
+++ Notes-02-20/main.c
@@ -7,10 +7,32 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     //Small change
-    printf("Hello, World!\n");
+    char* a = "Hello";
+    char b[5];
+    printf("a:%p\n",a);
+    printf("b:%p\n",b);
+    
+    char* e = (char*)malloc(sizeof(char)*5);
+    char* c = (char*)malloc(sizeof(char)*6);
+    char* d = (char*)malloc(sizeof(char)*5);
+    c[0] = 'h';
+    c[1] = 'e';
+    c[2] = 'l';
+    c[3] = 'l';
+    c[4] = 'o';
+    c[5] = 0;
+    printf("c:%p\n", c);
+    printf("The string is %s\n", c);
+    printf("The location of d %p\n", d);
+    printf("The location of e %p\n", e);
+    free(c);
+    free(e);
+    free(d);
+    
     return 0;
 }
