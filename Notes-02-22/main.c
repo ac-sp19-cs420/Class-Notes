798a6fd3ca68348c6205f5aab0c9be1b350c4c25
Aaron Block <ablock@austincollege.edu>
2019-02-22 09:43:36 -0600

    Printf

--- Notes-02-22/main.c
+++ Notes-02-22/main.c
@@ -11,24 +11,21 @@
 #include <string.h>
 #include <math.h>
 
-typedef struct point{
-    double x;
-    double y;
-} point;
-
-double origin_distance(point* p){
-    
-    double dis = pow(pow(p->x, 2.0) + pow(p->y, 2.0), 0.5);
-    return dis;
-}
-
 int main(int argc, const char * argv[]) {
-
-    point* p = (point*)malloc(sizeof(point));
-    p->x = 20;
-    p->y = 30;
-    double d = origin_distance(p);
-    printf("The value is %.2f\n",d);
+    int my_int, my_int2,my_int3;
+    double my_double, my_double2,my_double3;
+    char* str = "4 3.1";
     
-    return 0;
+    printf("Please enter a int and double:\n");
+    scanf("%d %lf",&my_int,&my_double);
+    printf("Please enter a string:\n");
+    
+    printf("Now seperate by / int and double:\n");
+    scanf("%d/%lf",&my_int2,&my_double2);
+    
+    sscanf(str, "%d %lf",&my_int3,&my_double3);
+    
+    printf("You entered: %d, %lf\n", my_int, my_double);
+    printf("You entered: %d, %lf\n", my_int2, my_double2);
+    printf("You entered: %d, %lf\n", my_int3, my_double3);
 }
