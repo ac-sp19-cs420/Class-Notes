e744442ed0d65a1d98f6b3d279731d599a7488a9
Aaron Block <ablock@austincollege.edu>
2019-02-22 09:29:27 -0600

    Command line parameters

--- Notes-02-22/main.c
+++ Notes-02-22/main.c
@@ -7,9 +7,15 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    printf("The number of params is %d\n", argc);
+    for(int i =0;i<argc;i++){
+        printf("Param %d is \"%s\"\n",i, argv[i]);
+        int z = atoi(argv[i]) + 20;
+        printf("Param %d plus 20 is %d\n",i, z);
+    }
     return 0;
 }
