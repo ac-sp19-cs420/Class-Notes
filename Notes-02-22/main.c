525486aaed761da6e3da25f2ad50e4876daf7afe
Aaron Block <ablock@austincollege.edu>
2019-02-22 09:06:56 -0600

    Initial Commit

---
+++ Notes-02-22/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Notes-02-22
+//
+//  Created by Aaron Block on 2/22/19.
+//  Copyright © 2019 ACCrew. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
