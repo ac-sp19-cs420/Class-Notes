de6071ac1f379e3e1c0628c222d75835377fb2c6
Aaron Block <ablock@austincollege.edu>
2019-02-22 11:30:53 -0600

    Final Notes

--- Notes-02-22/main.c
+++ Notes-02-22/main.c
@@ -12,20 +12,10 @@
 #include <math.h>
 
 int main(int argc, const char * argv[]) {
-    int my_int, my_int2,my_int3;
-    double my_double, my_double2,my_double3;
-    char* str = "4 3.1";
-    
-    printf("Please enter a int and double:\n");
-    scanf("%d %lf",&my_int,&my_double);
-    printf("Please enter a string:\n");
-    
-    printf("Now seperate by / int and double:\n");
-    scanf("%d/%lf",&my_int2,&my_double2);
-    
-    sscanf(str, "%d %lf",&my_int3,&my_double3);
-    
-    printf("You entered: %d, %lf\n", my_int, my_double);
-    printf("You entered: %d, %lf\n", my_int2, my_double2);
-    printf("You entered: %d, %lf\n", my_int3, my_double3);
+    const char* f = "/Users/adblock/Desktop/temp.txt";
+    FILE *fp = fopen(f, "w");
+    putc('q', fp);
+    fclose(fp);
+    printf("Done!\n");
+    return 0;
 }
